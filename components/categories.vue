80475cb4a303704dab33284a4af804bcdb30edea
cmsmix <cmsmix@gmail.com>
2018-10-21 18:01:47 +0900

    update

--- components/categories.vue
+++ components/categories.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="categories-menu">
     <div>
-       <a href="https://github.com/srhise/nuxt-wordpress-pwa" rel="noopener">github</a>
-    </div>
-    <div>
        <nuxt-link to="/">home</nuxt-link>
     </div>
     <div v-for="(item, key, index) in categories" :key="index">
